8f5a4f04603cb18e8f54f8b37e31bd0ac6c2636f
mr-tamil <100066590+mr-tamil@users.noreply.github.com>
2023-01-06 20:24:30 +0530

    Update __init__.py

--- cnet/NLP/__init__.py
+++ cnet/NLP/__init__.py
@@ -1,6 +1,11 @@
+from cnet import codio
+
+
 def Process():
+    # imported library error support: nltk
     import nltk
-    nltk.download('omw-1.4')  #  imported library error support: nltk
+    with codio.PrintStatement(False):
+        nltk.download('omw-1.4')
 
     from . import process
     return process
