5c6909aa2ad87ba70472d98b4c717050d49bbabd
mr-tamil <100066590+mr-tamil@users.noreply.github.com>
2023-01-06 19:20:16 +0530

    Update codio.py

--- codeNet/codio.py
+++ codeNet/codio.py
@@ -1,41 +1,60 @@
 """codio: Code Toolkit for I/O"""
 
 
+# import libraries
 import os, sys, subprocess, importlib
 
 
+# libraries easy access method:---
+	
 # dill source direct call
 from dill import source
 
-# get file source
+# get file source code
 from dill.source import getsource
 
+# --------------------------------
 
+
+# terminal command call by subprocess
 def command_call(cmd: str):
 	subprocess.check_call(cmd.split())
 
 
-# Install Packag
+# install single package
 def install_package(name)->None:
     if importlib.util.find_spec(name) is None:
         subprocess.check_call([sys.executable, '-m', 'pip', 'install', name])
 
+
+# install list of packages
 def install_packages(names:['packages'])->None:
     for name in names:
         install_package(name)
 	
 
-# Disable
-def blockPrint():
+# disable print output
+def disablePrint():
     sys.stdout = open(os.devnull, 'w')
 
-# Restore
+
+# enable print output
 def enablePrint():
     sys.stdout = sys.__stdout__
 
-# Enable or Disable Print output 
+
+# eable or disable print output on with statement
 class PrintStatements:
+    '''
+    with PrintStatements(state=True):
+    	# will print
+    	
+    with PrintStatements(state=True):
+    	# will not print
+    '''
+    
     def __init__(self, state= True):
+        # :param: state: True or False
         self.state = state
     	
     def __enter__(self):
@@ -46,4 +65,3 @@ class PrintStatements:
         if not self.state:
             sys.stdout.close()
             sys.stdout = sys.__stdout__
-
