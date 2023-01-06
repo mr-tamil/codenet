e97db4c2d00fa018629cbcd6ee876a0ecac1e739
mr-tamil <100066590+mr-tamil@users.noreply.github.com>
2023-01-06 15:04:30 +0530

    Update __init__.py

--- codeNet/__init__.py
+++ codeNet/__init__.py
@@ -1,3 +1,11 @@
 from . import codio
-from . import NLP
+
+NLP_CREATED = None
+def NLP():
+    nlp = NLP_CREATED
+    if NLP_CREATED is None:
+        from . import NLP as nlp
+        NLP_CREATED = nlp
+    return nlp
+
 
