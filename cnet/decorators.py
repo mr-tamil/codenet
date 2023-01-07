7449a51e003e1249fede5fc4d2e3f13b747859c1
mr-tamil <100066590+mr-tamil@users.noreply.github.com>
2023-01-07 23:15:15 +0530

    Create decorators.py

---
+++ cnet/decorators.py
@@ -0,0 +1,58 @@
+'''decorators: to decorate the class or function'''
+
+
+# import package modules
+from . import codio
+from filemanager import FileManager as fm
+
+# import libraries
+import time, datetime
+from functools import wraps
+
+
+# runtime decorator
+def timed(function):
+    
+    @wraps(function)
+    def wrapper(*args, **kwargs):
+        
+        # runtime count
+	    before = time.time()
+	    
+	    # run main function
+	    value = function(*args, **kwargs)
+	    after = time.time()
+	    
+	    print(f"{function.__name__} runs: {after-before} seconds")
+	    return value
+    
+    return wrapper
+
+
+# log decorator
+def logged(function):  # beta version
+    fileObj = fm('file.log')
+    
+    @wraps(function)
+    def wrapper(*args, **kwargs):
+        
+        # run time count
+        before = time.time()
+        
+        # eun main function
+        value = function(*args, **kwargs)
+        after = time.time()
+        
+        # get file details for log
+        dtype = codio.typesof(function)
+        dtype = dtype[0] if len(dtype) else type(function)
+        name = codio.cfpname(function)
+        
+        content = f"{datetime.datetime.now()} :: {after-before} || <{dtype}> {name} :returns ;{value}\n"
+        
+        # append log
+        fileObj.append(content)
+        return value
+    
+    return wrapper
+
