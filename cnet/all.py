b2846aff000bef63a00ad6c1ba4d0a5a9253c797
mr-tamil <100066590+mr-tamil@users.noreply.github.com>
2023-01-12 09:44:33 +0530

    Create all.py

---
+++ cnet/all.py
@@ -0,0 +1,12 @@
+# main
+from cnet import *
+from cnet.filemanager import *
+from cnet.codio import *
+from cnet.decorators import *
+
+# wolf
+from cnet.wolf import *
+from cnet.wolf.Neuron import *
+from cnet.wolf.Flow import *
+from cnet.wolf.CodeFlow import *
+from cnet.wolf.Work import *
