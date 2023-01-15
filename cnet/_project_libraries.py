ae58ea339b97816dabc015ecd290388f9e4977ce
mr-tamil <100066590+mr-tamil@users.noreply.github.com>
2023-01-15 19:51:12 +0530

    Update and rename cnet/project-modules/default.txt to cnet/_project_libraries.py

---
+++ cnet/_project_libraries.py
@@ -0,0 +1,23 @@
+project = {
+  "default": [
+              'datetime',
+              'inspect',
+              'inspect.getsource',
+              'inspect.signature',
+              'IPython.display.display',
+              'os',
+              'sys',
+              'io',
+              'json',
+              'collections',
+              'shutil',
+              'subprocess',
+              'time',
+              'functools',
+              'dill.source',
+              'IPython',
+              ('numpy', 'np'),
+              ('pandas', 'pd'),
+  ]
+             
+}
