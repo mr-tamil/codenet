f32b23fd07b7e8fb0eaa72206c3f87a7c868e44a
mr-tamil <100066590+mr-tamil@users.noreply.github.com>
2023-01-15 11:55:17 +0530

    Create libraries.py

---
+++ cnet/libraries.py
@@ -0,0 +1,40 @@
+from cnet import codio
+
+
+
+# import not installabled libraries
+
+# import ---
+import datetime
+import inspect
+import os
+import sys
+import io
+import json
+import collections
+import shutil
+import subprocess
+import time
+import functools
+
+
+# from ---
+from inspect import getsource, signature
+
+# _______________________________________________________________
+
+# import not installabled libraries
+# install packages
+packages = ['numpy', 'pandas', 'dill', 'IPython']
+codio.install_packages(packkages)
+
+
+# import ---
+import numpy as np
+import pandas as pd
+
+
+# from ---
+from IPython.display import display
+from dill import source
+
