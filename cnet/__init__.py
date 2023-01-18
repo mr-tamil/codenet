eb28fcba3eedb5ea7d807378ac96a43cababa9c6
mr-tamil <100066590+mr-tamil@users.noreply.github.com>
2023-01-18 13:47:22 +0530

    Update __init__.py

--- cnet/__init__.py
+++ cnet/__init__.py
@@ -11,34 +11,9 @@ from . import wolf
 
 # configuration file default data
 config_data = {
-
 }
 
-# cnet configuration data folder handler
-class CnetConfiguration:
-    def __init__(self):
-        self.parent_folder_name = '.cnet'
-        self.parent_folder_path = codio.os.path.join(codio.os.getcwd(), self.parent_folder_name)
-        if not codio.os.path.exists(self.parent_folder_path):
-            codio.os.mkdir(self.parent_folder_path)
-        
-        self.config_file_name = 'config.json'
-    
-    @property
-    def mainfile(self):
-        return self.getfile(self.config_file_name)
-
-    def getfile(self, filename):
-        get_file_path = codio.os.path.join(self.parent_folder_path, filename)
-        if not codio.os.path.exists(get_file_path):
-            file = FileManager(get_file_path)
-            file.clear()
-            return file
-        else:
-            return filemanager.FileManager(get_file_path)
-
-
-__ccfile = CnetConfiguration()
+__ccfile = codio.CnetConfiguration()
 __ccfile.mainfile.append(config_data)
 
     
