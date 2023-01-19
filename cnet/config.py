46394a07c0db1515e0347df326ddd8ea37a8ecee
mr-tamil <100066590+mr-tamil@users.noreply.github.com>
2023-01-19 11:34:43 +0530

    Update config.py

--- cnet/config.py
+++ cnet/config.py
@@ -1,5 +1,31 @@
-from . import codio
+import os
 from . import filemanager
+
+
+# cnet configuration data folder handler
+class BasicConfiguration:
+    def __init__(self, parentfoldername='', mainfilename='config.json'):
+        self.parent_folder_name = parentfoldername
+        self.parent_folder_path = os.path.join(os.getcwd(), self.parent_folder_name)
+        if not os.path.exists(self.parent_folder_path):
+            os.mkdir(self.parent_folder_path)
+        
+        self.config_file_name = mainfilename
+    
+    @property
+    def mainfile(self):
+        return self.getfile(self.config_file_name)
+
+    def getfile(self, filename):
+        get_file_path = os.path.join(self.parent_folder_path, filename)
+        if not os.path.exists(get_file_path):
+            file = filemanager.FileManager(get_file_path)
+            file.clear()
+            return file
+        else:
+            return filemanager.FileManager(get_file_path)
+
+
 
 # configuration file default data
 __config_data = {
@@ -9,7 +35,10 @@ __config_data = {
     }
 }
 
-__ccfile = codio.CnetConfiguration()
-__ccfile.mainfile.append(__config_data)
+cnetconfig = BasicConfiguration('.cnet', 'config.json')
 
-    
+def reset():
+    cnetconfig.mainfile.write(__config_data)
+
+if not os.path.exists(os.path.join(cnetconfig.parent_folder_path, 'config.json')):
+    reset()
